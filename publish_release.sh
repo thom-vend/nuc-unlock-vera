cd787dbc0135cae9079b18851e1312015aa2e2b0
Thom'H <thomsh@vendhq.com>
2024-04-12 12:59:35 +1200

    fix publish_release.sh

--- publish_release.sh
+++ publish_release.sh
@@ -8,8 +8,8 @@ if [[ -z "$new_tag" ]] || [[ $# -ne 1 ]]; then
 fi
 go fmt
 ./go_tests.sh
-./go_build_windows.sh
 ./integration_tests.sh
-git tag -a "$new_tag"
+./go_build_windows.sh
+git tag -a "$new_tag" -m 'Release tag'
 git push origin "$new_tag"
 gh release create --generate-notes --latest "$new_tag" ./release/*
