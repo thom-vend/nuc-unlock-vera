19c265af12866a53f89c046ecc0a151fca1d2efa
Thom'H <thomsh@vendhq.com>
2024-04-12 12:55:25 +1200

    Update publish_release.sh

--- publish_release.sh
+++ publish_release.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set -euxo pipefail
-new_tag=${1}
-comment=${2}
+git tag -l
+new_tag=${1:-}
+if [[ -z "$new_tag" ]] || [[ $# -ne 1 ]]; then
+    echo "Error: usage :$0 <tag>"
+    exit 1
+fi
+go fmt
+./go_tests.sh
 ./go_build_windows.sh
-git tag -a "$new_tag" -m "$comment"
+./integration_tests.sh
+git tag -a "$new_tag"
 git push origin "$new_tag"
 gh release create --generate-notes --latest "$new_tag" ./release/*
